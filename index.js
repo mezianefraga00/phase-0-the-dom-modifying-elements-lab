09bbab5987a12bcaba12fce7acbd3ca545aca23f
mezianefraga00 <mezianefraga00@gmail.com>
2021-03-15 23:34:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here!]
+
+const dr = document.getElementsByTagName('main')[0]
+dr.remove();    
+var newHeader = document.getElementsByTagName("H1")[0];
+
